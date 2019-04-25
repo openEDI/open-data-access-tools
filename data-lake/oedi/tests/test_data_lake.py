884292d486da6f24726f43758a6a4a61889f614c
David Rager <david.rager@nrel.gov>
2019-04-25 16:16:15 -0600

    New tests for pv_rooftops_buildings table

--- data-lake/oedi/tests/test_data_lake.py
+++ data-lake/oedi/tests/test_data_lake.py
@@ -5,10 +5,11 @@ from pprint import pprint
 
 
 client = boto3.client('athena')
+gclient = boto3.client('glue')
 
 class TestClass(object):
 
-    def test_tracking_the_sun(self):
+    def test_tracking_the_sun_query(self):
         response = client.start_query_execution(
             #QueryString='SELECT "system size" * FROM "oedidb"."oedi_tracking_the_sun" where "sales tax cost" = 725.386027;',
             QueryString = 'SELECT "system size" FROM "oedidb"."oedi_tracking_the_sun" where "sales tax cost" = 725.386027',
@@ -53,5 +54,83 @@ class TestClass(object):
 
         assert theAnswer == '7.811037539'
 
-a = TestClass()
-a.test_tracking_the_sun()
+    def test_pv_rooftops_buildings_query(self):
+        response = client.start_query_execution(
+
+            QueryString = "SELECT region_id FROM oedidb.pv_rooftops_buildings where gid = 3120 and city_year = 'pierre_sd_08'",
+            QueryExecutionContext={
+                'Database': 'oedidb'
+            },
+            ResultConfiguration={
+                'OutputLocation': 's3://aws-athena-query-results-501953089731-us-west-2/',
+                'EncryptionConfiguration': {
+                    'EncryptionOption': 'SSE_S3'
+                }
+            }
+        )
+        #print(response)
+
+        x = 0
+        theAnswer = ''
+        while x < 10:
+            try:
+                response3 = client.get_query_execution(
+                    QueryExecutionId=response['QueryExecutionId']
+                )
+                pprint(response3)
+                response2 = client.get_query_results(
+                    QueryExecutionId=response['QueryExecutionId']
+                )
+                #pprint(response2)
+                for row in response2['ResultSet']['Rows']:
+                    #print(row)
+                    if row['Data'][0]['VarCharValue'] != 'region_id':
+                        theAnswer =  row['Data'][0]['VarCharValue']
+                break
+            except Exception as e:
+                #print(e)
+                if 'QUEUED' in str(e):
+                    time.sleep(5)
+                    print('sleeping')
+                elif 'FAILED' in str(e):
+                    raise
+                else:
+                    time.sleep(5)
+        print(theAnswer)
+
+        assert theAnswer == '118'
+
+    def test_pv_rooftops_buildings_partitions(self):
+        count = 0
+        response = gclient.get_partitions(
+
+            DatabaseName='oedidb',
+            TableName='pv_rooftops_buildings'
+
+        )
+
+        pprint(response)
+        next_token = response['NextToken']
+        pprint(next_token)
+
+        for part in response['Partitions']:
+            count = count + 1
+
+        print("NEXT")
+        response2 = gclient.get_partitions(
+
+            DatabaseName='oedidb',
+            TableName='pv_rooftops_buildings',
+            NextToken=next_token
+        )
+        pprint(response2)
+        for part in response2['Partitions']:
+            count = count + 1
+
+        print(count)
+        # TODO: Insure this is actually supposed to be the correct count... :)
+        assert count == 168
+
+
+#a = TestClass()
+#a.test_pv_rooftops_buildings_partitions()
