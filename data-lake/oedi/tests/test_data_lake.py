63fce5f0d10c4c98234cc61d117f1e22d348a6a0
David Rager <david.rager@nrel.gov>
2019-04-26 12:34:00 -0600

    New test added for partitions on rooftops

--- data-lake/oedi/tests/test_data_lake.py
+++ data-lake/oedi/tests/test_data_lake.py
@@ -11,7 +11,6 @@ class TestClass(object):
 
     def test_tracking_the_sun_query(self):
         response = client.start_query_execution(
-            #QueryString='SELECT "system size" * FROM "oedidb"."oedi_tracking_the_sun" where "sales tax cost" = 725.386027;',
             QueryString = 'SELECT "system size" FROM "oedidb"."oedi_tracking_the_sun" where "sales tax cost" = 725.386027',
             QueryExecutionContext={
                 'Database': 'oedidb'
