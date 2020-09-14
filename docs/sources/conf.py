3fe94bd697a1418a738450de3cddd1a929ddab74
jgu2 <jianli.gu@nrel.gov>
2020-09-14 12:18:31 -0600

    Move rst to sources dir and update docs

--- docs/sources/conf.py
+++ docs/sources/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'OEDI'
-copyright = '2020, NREL'
+copyright = '2020 Alliance for Sustainable Energy, LLC'
 author = 'NREL'
 
 # The full version, including alpha/beta/rc tags
@@ -52,4 +52,4 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../_static']
